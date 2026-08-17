291eeeb71b43900edb9bf4d38fc3f7d9b06e9abf
Josh Nisenson <josh803316@gmail.com>
2026-08-16 22:13:25 -0300

    chore(deps): upgrade libraries to latest compatible versions (#4)

--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-import sharedConfig from '@josh803316/shared-config/eslint.config.js';
-
-export default [
-  ...sharedConfig,
-  {ignores: ['dist/', 'node_modules/', 'playwright-report/', 'test-results/']},
-];
